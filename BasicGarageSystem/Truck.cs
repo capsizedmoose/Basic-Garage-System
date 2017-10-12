798df3c9667771927dfca40c288d4e07776ccd8c
Sinan <sinanrad@outlook.com>
2017-10-12 11:31:25 +0200

    Added Truck Class

---
+++ BasicGarageSystem/Truck.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicGarageSystem
+{
+    class Truck:Vehicle
+    {
+        public Truck()
+        {
+            vehicleType = "Truck";
+            regNr = " ";
+            dateTime = " ";
+            parkingSpot = ' ';
+            vehicleSize = '1';
+        }
+    }
+}
